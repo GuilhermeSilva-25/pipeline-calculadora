2ed32dd335811f639c8962fd735f0b839997c7b3
Guilherme Silva <guime.silva333@gmail.com>
2026-08-26 22:50:18 -0300

    fix: arruma teste de divisao e ajusta gitleaks

--- test_divisao.py
+++ test_divisao.py
@@ -3,3 +3,7 @@ from calculadora import divisao
 
 def test_divisao():
     assert divisao(10, 2) == 5
+
+
+def test_divisao_por_zero():
+    assert divisao(10, 0) == "Não é possível dividir por zero."
